1001eb6ac790ebaa9a7aa663e59986682a0e1e1e
Your Name <you@example.com>
2021-10-24 03:10:49 -0400

    first commit

--- src/main/java/com/apress/todo/config/ToDoConfig.java
+++ src/main/java/com/apress/todo/config/ToDoConfig.java
@@ -1,9 +1,20 @@
 package com.apress.todo.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+
+import com.apress.todo.interceptor.ToDoMetricInterceptor;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @EnableConfigurationProperties(ToDoProperties.class)
 @Configuration
 public class ToDoConfig {
+    @Bean
+    public MappedInterceptor metricInterceptor(MeterRegistry registry) {
+       return new MappedInterceptor(new String[]{"/**"},
+                        new ToDoMetricInterceptor(registry));
+    }
 }
